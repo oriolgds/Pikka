2a5f3ead5264e1b754d9fd5c3fca3535c83e8f0c
oriolginger2008 <oriolginger2008@gmail.com>
2024-06-29 18:06:16 +0000

    Bootstrap icons added

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,7 +1,13 @@
 {pkgs}: {
   channel = "stable-23.11";
+  # Use https://search.nixos.org/packages to find packages
   packages = [
+    #pkgs.go
+    #pkgs.python311
+    #pkgs.python311Packages.pip
     pkgs.nodejs_20
+    pkgs.nodePackages.nodemon
+    pkgs.unzip
   ];
   idx.extensions = [
     "angular.ng-template"
