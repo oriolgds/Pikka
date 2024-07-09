bfeaa15368eb0890ab68d022b6bf3aa2157a77f2
oriolginger2008 <oriolginger2008@gmail.com>
2024-07-09 16:04:30 +0000

    404 page finished

--- src/app/not-found-page/not-found-page.component.ts
+++ src/app/not-found-page/not-found-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-not-found-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './not-found-page.component.html',
   styleUrl: './not-found-page.component.css'
 })
