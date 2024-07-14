9222c9947ad22d9b386b1cc49bb7ca87c7ed26a0
oriolginger2008 <oriolginger2008@gmail.com>
2024-07-14 20:45:24 +0000

    Carrousel managing errors And loading

--- src/app/main-page/carrousel/carrousel.component.ts
+++ src/app/main-page/carrousel/carrousel.component.ts
@@ -4,18 +4,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-carrousel',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatDividerModule, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './carrousel.component.html',
   styleUrl: './carrousel.component.css'
 })
 
 export class CarrouselComponent implements OnInit {
   refreshButtonText: string = 'Refresh';
-
+  fetched: boolean = false;
   meals: any[] = [];
   private apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'; // Replace with your API endpoint
 
@@ -26,6 +26,7 @@ export class CarrouselComponent implements OnInit {
   }
 
   async fetchImages(): Promise<any> {
+
     for (let i = 0; i < 5; i++) {
       try {
         const response = await fetch(this.apiUrl);
@@ -35,8 +36,10 @@ export class CarrouselComponent implements OnInit {
         const json = await response.json();
         this.meals.push(json.meals[0]);
       } catch (error) {
+        this.fetched = true;
         console.error('Error fetching images:', error);
       }
+      this.fetched = true;
     }
   }
   Reload(): void {
@@ -45,5 +48,5 @@ export class CarrouselComponent implements OnInit {
       window.location.reload();
     }, 2000);
   }
-  
+
 }
---
+++ www/pikka/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-YHS4O64J.mjs";import"./chunk-HIIO2DL2.mjs";import"./chunk-464BCLMC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
